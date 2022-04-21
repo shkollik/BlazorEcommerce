0b5d9211c793b300ddbcb18f7770f73eab19c8a9
Vladyslav Shkola <shkolik92@gmail.com>
2022-04-21 20:08:41 +0300

    Add project files.

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "The Hitchhiker's Guide to the Galaxy",
+                    Description = "The Hitchhiker's Guide to the Galaxy[note 1] (sometimes referred to as HG2G,[1] HHGTTG,[2] H2G2,[3] or tHGttG) is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including stage shows, novels, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                    Price = 9.99m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Ready Player One",
+                    Description = "eady Player One is a 2018 American science fiction film based on Ernest Cline's novel of the same name. Directed by Steven Spielberg, from a screenplay by Zak Penn and Cline, it stars Tye Sheridan, Olivia Cooke, Ben Mendelsohn, Lena Waithe, T.J. Miller, Simon Pegg, and Mark Rylance. Set in 2045, much of humanity uses the OASIS, a virtual reality simulation, to escape the real world. The orphaned Wade Watts finds clues to a contest that promises the ownership of the OASIS to the winner, and he and his allies try to complete it before an evil corporation can do so.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png",
+                    Price = 7.99m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Nineteen Eighty-Four",
+                    Description = "Nineteen Eighty-Four (also stylised as 1984) is a dystopian social science fiction novel and cautionary tale written by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
+                    Price = 8.99m
+                }
+            );
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
