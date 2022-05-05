f5e5bf460aa89ce63c768c39e3d0acef4ad27bdc
Vladyslav Shkola <shkolik92@gmail.com>
2022-05-05 15:05:01 +0300

    product details seeding

--- BlazorEcommerce/Shared/ServiceResponse.cs
+++ BlazorEcommerce/Shared/ServiceResponse.cs
@@ -5,5 +5,7 @@
         public T? Data { get; set; }
         public bool Success { get; set; } = true;
         public string Error { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
     }
 }
